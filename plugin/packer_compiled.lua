ba73eabbf7d642dc880e4d93f27a26698dc14574
Madelaine Dalangin <madelainedalangin@Madelaines-Macbook-Pro.local>
2023-03-29 22:01:21 -0600

    edited ducks plugin

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -175,11 +175,6 @@ _G.packer_plugins = {
     path = "/Users/madelainedalangin/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
-  ["null-ls.nvim"] = {
-    loaded = true,
-    path = "/Users/madelainedalangin/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
-    url = "https://github.com/jose-elias-alvarez/null-ls.nvim"
-  },
   ["nvim-autopairs"] = {
     loaded = true,
     path = "/Users/madelainedalangin/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
