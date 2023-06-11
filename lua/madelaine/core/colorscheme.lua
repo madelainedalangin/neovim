241b37d21cb11a8197b373db81182a2975b439f6
Madelaine Dalangin <madelainedalangin@Madelaines-Macbook-Pro.local>
2023-06-11 12:57:46 -0600

    changed theme to everforest

--- lua/madelaine/core/colorscheme.lua
+++ lua/madelaine/core/colorscheme.lua
@@ -1,6 +1,12 @@
 --vim.cmd("colorscheme nightfly")
 
-local status, _ = pcall(vim.cmd, "colorscheme nightfly")
+--local status, _ = pcall(vim.cmd, "colorscheme nightfly")
+--if not status then
+--	print("Color scheme not found!")
+--	return
+--end
+
+local status, _ = pcall(vim.cmd("colorscheme everforest"))
 if not status then
 	print("Color scheme not found!")
 	return
