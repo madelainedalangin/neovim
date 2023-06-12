2960c56c2a1d9d1c92025d5f4d30d6ee05a92d58
Madelaine Dalangin <madelainedalangin@Madelaines-Macbook-Pro.local>
2023-06-12 02:59:05 -0600

    added extra config for gruvbox

--- lua/madelaine/core/colorscheme.lua
+++ lua/madelaine/core/colorscheme.lua
@@ -11,7 +11,29 @@
 --	print("Color scheme not found!")
 --	return
 --end
-
+vim.o.background = "dark"
+require("gruvbox").setup({
+	undercurl = true,
+	underline = true,
+	bold = true,
+	italic = {
+		strings = true,
+		comments = true,
+		operators = false,
+		folds = true,
+	},
+	strikethrough = true,
+	invert_selection = false,
+	invert_signs = false,
+	invert_tabline = false,
+	invert_intend_guides = false,
+	inverse = true, -- invert background for search, diffs, statuslines and errors
+	contrast = "medium", -- can be "hard", "soft" or empty string
+	palette_overrides = {},
+	overrides = {},
+	dim_inactive = false,
+	transparent_mode = false,
+})
 local status, _ = pcall(vim.cmd("colorscheme gruvbox"))
 if not status then
 	print("Color scheme not found")
